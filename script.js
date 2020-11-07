fe89ed7a1ee7aa41a246ff7daeb05dd74a4c2187
Lisa Lee Källman <lisaleekallman@Lisas-MacBook-Air.local>
2020-11-07 12:02:18 +0100

    added hover effect on project links, added color letter project

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
 
-// resize header
-window.onscroll = function() {scrollFunction()};
+// // resize header
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    document.getElementById('logo').style.fontSize = '30px';
-  } else {
-    document.getElementById('logo').style.fontSize = '67px';
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+//     document.getElementById('logo').style.fontSize = '30px';
+//   } else {
+//     document.getElementById('logo').style.fontSize = '67px';
+//   }
+// }
 
 
 // book
 const two = document.querySelector('.two');
-  two.addEventListener('mousemove', function(event) {
-	const x = event.pageX;
-	const y = event.pageY;
-	const midX = x - window.innerWidth / 2;
-	const midY = y - window.innerHeight / 2;
-
-	const book = document.querySelector('.book')
-	book.style.left = x + 'px';
-	book.style.top = y + 'px';
-
-	book.style.transform = 'rotateX(' + (midY * 0.5) + 'deg)rotateY(' + midX + 'deg)';
+two.addEventListener('mousemove', function(event) {
+  const x = event.pageX;
+  const y = event.pageY;
+  const midX = x - window.innerWidth / 2;
+  const midY = y - window.innerHeight / 2;
+  
+  const book = document.querySelector('.book')
+  book.style.left = x + 'px';
+  book.style.top = y + 'px';
+  
+  book.style.transform = 'rotateX(' + (midY * 0.5) + 'deg)rotateY(' + midX + 'deg)';
 })
-
-
-// re-size text on scroll
-// $(document).on("scroll", function() {
-//     var pixels = $(document).scrollTop()
-//     var font = pixels /100 + 12
-//     $(".six").css("font-size", font)
-
-//     $("span").html(font)
-// });
 
 // 
 // // a 
@@ -51,7 +41,7 @@ const two = document.querySelector('.two');
 // const sketch01 = function (graphic) {
 //     graphic.setup = function() {
 //         graphic.createCanvas(200, 200, graphic.WEBGL);
-        
+
 //         graphic.fill('#fff')
 //         graphic.textFont('helvetica')
 //         // graphic.textAlign(CENTER, CENTER)
@@ -59,10 +49,10 @@ const two = document.querySelector('.two');
 //         graphic.text('a', 100, 100)
 //     }
 //     // const container = document.querySelector('.eight');
-    
+
 //     // const canvas1 = document.getElementById('defaultCanvas0');
 //     // container.appendChild(canvas1);//not working
-    
+
 //     graphic.draw = function() {
 //         graphic.background("#000")
 //         const tileSize = 25
@@ -74,7 +64,7 @@ const two = document.querySelector('.two');
 //                 const sy = y * tileSize
 //                 const sw = tileSize
 //                 const sh = tileSize
-                
+
 //                 //destination
 //                 const dx =  x * tileSize
 //                 const dy = y * tileSize
@@ -89,7 +79,7 @@ const two = document.querySelector('.two');
 // const sketch02 = function (graphic) {
 //     graphic.setup = function(){
 //         graphic.createCanvas(200, 100, graphic.WEBGL);
-        
+
 //         graphic.fill("#004d9d")
 //         graphic.textSize(16)
 //         // graphic.textAlign(CENTER, CENTER)
@@ -98,13 +88,13 @@ const two = document.querySelector('.two');
 //         graphic.text("Good\nMorning", 100, 50)
 //     }
 //     // const container2 = document.querySelector('.twelve');
-    
+
 //     //   const canvas2 = document.getElementById('defaultCanvas0');
 //     //   container.appendChild(canvas1);//not working
 
 //     graphic.draw = function() {
 //         graphic.background("#e23f12")
-        
+
 //         const tileSize = 10
 //         for (let x = 0; x < 20; x = x + 1) {
 //             for (let y = 0 ; y < 10; y = y + 1) {  
@@ -132,7 +122,7 @@ document.querySelector("textarea").addEventListener("keyup", function(){
   // function setup(){
   //   createCanvas (200, 100)
   //   graphic2 = createGraphics(200, 100)
-    
+  
   //   graphic2.fill("#004d9d")
   //   graphic2.textSize(16)
   //   graphic2.textAlign(CENTER, CENTER)
@@ -141,18 +131,18 @@ document.querySelector("textarea").addEventListener("keyup", function(){
   //   graphic2.text("Good\nMorning", 100, 50)
   
   //   const container2 = document.querySelector('.twelve');
-    
+  
   //   const canvas2 = document.getElementById('defaultCanvas1');
   //   container2.appendChild(canvas2);
   // }
   
   // function draw(){
   //   background("#e23f12")
-    
+  
   //   const tileSize = 10
   //   for (let x = 0; x < 20; x = x + 1) {
   //     for (let y = 0 ; y < 10; y = y + 1) {
-        
+  
   //     }
   //   }
   //   image(graphic2, frameCount, 0, 200, 100)
@@ -190,8 +180,8 @@ document.querySelector("textarea").addEventListener("keyup", function(){
       div.style.fontWeight = 100 + (200 * Math.round(8 * score));
     });
   });
-
-
+  
+  
   // clock
   function runClock() {
     let now = new Date();
@@ -199,30 +189,30 @@ document.querySelector("textarea").addEventListener("keyup", function(){
     let min = now.getMinutes();
     let sec = now.getSeconds();
     let ms = now.getMilliseconds();
-
+    
     const clock = document.querySelector('.clock');
     const hourHand = clock.querySelector('.hour');
     const minHand = clock.querySelector('.minute');
     const secHand = clock.querySelector('.second');
-
+    
     let hourRotation = 360/12 * hour + 0.5 * min;
     let minRotation = 6 * min + 0.1 * sec;
     let secRotation = 6 * sec + 0.006 * ms;
-
+    
     hourHand.style.transform = 'rotate(' + hourRotation + 'deg)';
     minHand.style.transform = 'rotate(' + minRotation + 'deg)';
     secHand.style.transform = 'rotate(' + secRotation + 'deg)';
-
-
+    
+    
     requestAnimationFrame(runClock);
   }
-
+  
   runClock();
-
-
+  
+  
   // who I am
   let phrases = [
-  
+    
     'I’m a digital creative and Frontend Developer..',
     'I’m a digital creative and Frontend Developer based in Stockholm.',
     'I’m a digital creative and Frontend Developer based in Stockholm. I\m a disguised nerd..',
@@ -230,33 +220,58 @@ document.querySelector("textarea").addEventListener("keyup", function(){
     'I’m a digital creative and Frontend Developer based in Stockholm. I\m a nerd. So, I had to learn code. I started self-studying web design and code in the spring of 2020.',
     
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019.',
-      
+    
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island.',
     
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of web design and graphic design.',
-      
+    
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of responsive web design, graphic design and art direction.',
     
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of responsive web design, graphic design and art direction. I come from the field of urban planning and architecture.',
-      
+    
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of responsive web design, graphic design and art direction. I recently transitioned from the field of urban planning and sustainable architecture.',
-      
+    
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of responsive web design, graphic design and art direction. I recently transitioned from the field of urban planning and sustainable architecture. I have a strong conviction that inclusion and diversity are key for qualitative and sustainable design.',
-      
+    
     'I’m a digital creative and Frontend Developer based in Stockholm. I started self-studying web design and code in the spring of 2019. Currently, I\m studying Frontend Developer at Hyper Island. I work in the fields of responsive web design, graphic design and art direction. I recently transitioned from the field of urban planning and sustainable architecture. I have a strong conviction that inclusion and diversity, throughout the process, are key for qualitative design, leading to both business growth, societal prosperity and indivudual growth.'
-      ]
-    $("input").on("input", function (){
-      let value = $(this).val()
-      let phrase = phrases[value]
-      $("div.phrase").html(phrase)
+  ]
+  $("input").on("input", function (){
+    let value = $(this).val()
+    let phrase = phrases[value]
+    $("div.phrase").html(phrase)
+  })
+  
+  
+  
+  // contacts
+  'use strict'
+  
+  // create spans for every letter in contacts div
+  function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr) }
+  
+  function makeSpans (selector) {
+    var _document$querySelect = document.querySelectorAll(selector)
+    var _document$querySelect2 = _toArray(_document$querySelect)
+    var elements = _document$querySelect2.slice(0)
+    
+    return elements.map(function (element) {
+      var text = element.innerText.split('')
+      var spans = text.map(function (letter) {
+        return '<span>' + letter + '</span>'
+      }).join('')
+      return element.innerHTML = spans
     })
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  }
+  
+  // usage with single selector
+  makeSpans('.contacts')
+  
+  // // or with multiple selectors
+  // makeSpans('h1, h2, .heading')
+  
+  
+  
+  
+  
+  
+  
